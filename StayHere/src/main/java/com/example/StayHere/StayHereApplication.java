b5108ec276d2bbe5721860ac1a5881ba9ba31b0d
ivan <ivan01sanfer@gmail.com>
2023-04-11 16:38:52 +0200

    v1

---
+++ StayHere/src/main/java/com/example/StayHere/StayHereApplication.java
@@ -0,0 +1,13 @@
+package com.example.StayHere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayHereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayHereApplication.class, args);
+	}
+
+}
